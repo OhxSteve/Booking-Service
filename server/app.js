f597ac74a5bdc8ddd94b10f3ea514e9a865ae781
Steve Oh <OhxSteve@gmail.com>
2018-08-20 12:14:14 -0700

    Implement Node Clustering

--- server/app.js
+++ server/app.js
@@ -1,27 +1,49 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cluster = require('cluster');
 const routes = require('./../routes');
+
 require('newrelic');
 
 const app = express();
 
-app.set('port', process.env.PORT || 3001);
+if (cluster.isMaster) {
+  const numWorkers = require('os').cpus().length;
 
-app.use(bodyParser.json());
+  console.log(`Master cluster setting up ${numWorkers} workers...`);
 
-app.get('/', (req, res) => {
-  res.redirect('/rooms/1');
-});
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
 
-app.use(express.static('public/'));
-app.use(express.static('client/dist'));
+  cluster.on('online', (worker) => {
+    console.log(`Worker ${worker.process.pid} is online`);
+  });
 
-app.get('/rooms/:id', (req, res) => {
-  const reactPath = path.join(__dirname, '../public/index.html');
-  res.sendFile(reactPath);
-});
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+} else {
+  app.set('port', process.env.PORT || 3001);
 
-app.use('/api', routes);
+  app.use(bodyParser.json());
+
+  app.get('/', (req, res) => {
+    res.redirect('/rooms/1');
+  });
+
+  app.use(express.static('public/'));
+  app.use(express.static('client/dist'));
+
+  app.get('/rooms/:id', (req, res) => {
+    const reactPath = path.join(__dirname, '../public/index.html');
+    res.sendFile(reactPath);
+  });
+
+  app.use('/api', routes);
+}
 
 module.exports = app;
